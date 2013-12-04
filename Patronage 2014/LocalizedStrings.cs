8fa2feb76779e7f6e10b3187aa54cffb67b71770
Szymon Cywinski <sz.cywinski@gmail.com>
2013-12-04 20:23:05 +0100

    added empty project

---
+++ Patronage 2014/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Patronage_2014.Resources;
+
+namespace Patronage_2014
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
