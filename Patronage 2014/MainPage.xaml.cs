b5c85cf87a6fd1115643a28cb90aae4ad1a474b2
Szymon Cywinski <sz.cywinski@gmail.com>
2013-12-06 13:36:11 +0100

    deleted unnecessary comments and auto generated code

--- Patronage 2014/MainPage.xaml.cs
+++ Patronage 2014/MainPage.xaml.cs
@@ -13,7 +13,7 @@ namespace Patronage_2014
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+        
         public MainPage()
         {
             InitializeComponent();
@@ -38,20 +38,5 @@ namespace Patronage_2014
             NavigationService.Navigate(new Uri("/AddNewPersonPage.xaml", UriKind.Relative));
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
