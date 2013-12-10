0a1e00ca55153e9a95930ac2fe13d47e33ed872b
Szymon Cywinski <sz.cywinski@gmail.com>
2013-12-10 14:58:28 +0100

    Added model class

---
+++ Patronage 2014/Model/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patronage_2014.Model
+{
+    public class Student
+    {
+        private String firstName;
+        private String lastName;
+        private Decimal grade;
+
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public Decimal Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+    }
+}
